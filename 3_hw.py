93bef40db658054dd1176023d6e5ccdda4fdcc3c
jeun9e <urazgaliev@list.ru>
2025-01-13 19:31:32 +0300

    Update 3_hw.py

--- 3_hw.py
+++ 3_hw.py
@@ -1,24 +1,24 @@
 # Задача 1 наибольшее число
-def large(a,b):
+def max_number(a,b):
     if a > b:
         print(a)
     elif b > a:
         print(b)
-large(150,20)
+max_number(150,20)
 
 
 # Задача 2 разница в 135
-def Zad2(a,b):
+def task_2(a,b):
     if abs(a-b) == 135:
         print('Yes')
     else:
          print('No')
 
-Zad2(0,135)
+task_2(0,135)
 
 
 # Задача 3 название сезона
-def zad3(month):
+def time_of_the_year(month):
     if month == 12 or month == 1 or month == 2:
         print('Winter')
     elif month == 3 or month == 4 or month == 5:
@@ -27,29 +27,29 @@ def zad3(month):
         print('Summer')
     elif month in range (9,12):
         print('Autumn')
-zad3(9)
+time_of_the_year(9)
 
 
 # Задача 4 числа больше 10
-def zad4(a,b,c):
+def more_ten(a,b,c):
     if a>10 and b>10 and c>10:
         print('Yes')
     else:
         print('No')
-zad4(11,12,30)
+more_ten(11,12,30)
 
 
 #Задача 5 количество положительных чисел
-def zad5(a: list):
+def positive_number(a: list):
     sum = 0
     for i in a:
         if i > 0:
             sum = sum + 1
     return sum
-print(zad5([1,-2,3,-4,5]))
+print(positive_number([1,-2,3,-4,5]))
 
-# Задача 6 вывести в количество дней
-def zad6(a: int,b: int):
+# Задача 6 вывести количество дней
+def sum_day(a: int,b: int):
     return (a*12*29)+(b*29)
-print(zad6(0,10))
+print(sum_day(0,10))
 
